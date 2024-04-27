3db9fe0aeea6d848cf377ac5ddbbe540e9b4bfc6
EasySwap <142835812+EziSwap@users.noreply.github.com>
2024-04-27 23:19:49 +0300

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-d34b4f18-a10d-40ae-b30e-5da6e4d2c3be"
 
 OUTPUT_DIR="logs"
 
-axelar_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
+UA=$(shuf -n 1 ua.txt)
+
+axelar_data=$(curl -A UA -x http://premium.proxywing.com:12321 --proxy-user d9hv1twu6q:rzld6kr3mb -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
 
 if [ $? -eq 0 ]; then
     if [ ! -d "$OUTPUT_DIR" ]; then
         mkdir "$OUTPUT_DIR"
     fi
-    
+
     echo "$axelar_data" >> "$OUTPUT_DIR/axelar.txt"
-    
+
     echo "Axelar data fetched and saved to $OUTPUT_DIR/axelar.txt."
 else
     echo "Failed to fetch Axelar data."
